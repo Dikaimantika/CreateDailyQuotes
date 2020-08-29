a77276a7b551c023e7a9d33b9ccbfeb12d3b9bcc
Dikaimantika <dika01imantika@gmail.com>
2020-08-29 13:51:47 +0700

    fourth commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var arrQuotes = [];
-var current = 3;
+var current = 0;
 
 processQuote();
 
@@ -7,11 +7,14 @@ function processQuote(){
     fetch("https://quotes-api.archv.id/api/quotes/v1/latest")
     .then(response => response.json())
     .then (result => {
-        
-        var arrQuotes = result.content;
-        document.getElementById("contentQuote").innerHTML = arrQuotes[current].content;
-        document.getElementById("authorQuote").innerHTML = arrQuotes[current].author;
+        arrQuotes = result.content;
+        output();
     })
+}
+
+function output(){
+    document.getElementById("contentQuote").innerHTML = arrQuotes[current].content;
+    document.getElementById("authorQuote").innerHTML = arrQuotes[current].author;
 }
 
 function previousButton(){
@@ -19,27 +22,27 @@ function previousButton(){
         alert("Ini Quote paling awal")
     } else{
         current = current-1;
-        processQuote();
+        output();
         changeBGround();
     }
 }
 
 function nextButton(){
     if(current == arrQuotes.length-1){
-        alert("Quote habis")
+        alert("Quote habis. Ini Quote terakhir")
     } else{
         current = current+1;
-    processQuote();
+    output();
     changeBGround();
     }
 }
 
- function random(number) {
-          return Math.floor(Math.random()*(number+1));
-        }
+function random(number) {
+        return Math.floor(Math.random()*(number+1));
+}
 
 function changeBGround(){
-    Math.floor(Math.random() * 7)
-    var sourceBg = 'url(../images/img'+ random(7)+ '.jpg)';
-    document.body.style.background = sourceBg;
+    Math.floor(Math.random() * 9)
+    var sourceBg = 'url(../images/img'+ random(9)+ '.jpg)';
+    document.getElementById("bg").style.backgroundImage = sourceBg;
 }
